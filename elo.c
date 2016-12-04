49cdb048c05e37eb8b3285b3fcd631c957fd54cc
Rob Cameron <rwcameron@gmail.com>
2016-12-04 14:32:29 -0800

    add func

---
+++ elo.c
@@ -0,0 +1,6 @@
+import "elo.h"
+
+int calc_elo(int player_a, int player_b) {
+  
+  return 0;
+}
--- elo.h
+++ elo.h
@@ -1,6 +1,6 @@
 #ifndef ELO_H
 #define ELO_H
 
-int calc_elo();
+int calc_elo(int player_a, int  player_b);
 
 #endif /* ELO_H */
